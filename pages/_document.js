6ed028afc59d1ad8c92542b4c645a5d2ad94e8a0
shiv-rishinarayan <shivam.rishinarayansingh@gmail.com>
2026-08-04 00:26:42 +0530

    Add Google Analytics (G-X2YVKN37Q5) to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-X2YVKN37Q5"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-X2YVKN37Q5');
+            `,
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
